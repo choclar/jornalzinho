cb905330b56dfc34ff7157fd2e7c38d37743b710
choclar <comercial@choclar.com.br>
2026-02-18 15:49:37 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-
+ 
 import React, { useState, useRef, useEffect } from 'react';
 import { PostConfig, TextTransform } from './types';
 import { DEFAULT_CONFIG, calculatePostDimensions } from './constants';
